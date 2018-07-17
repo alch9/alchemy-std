b142f7f5bcd31db4cd42d9c60273e29fdd684aef
Sudeep Jathar <sudeep.jathar@gmail.com>
2018-07-17 14:55:15 +0000

    Added runcmd with formatting

--- alchemy_std/shell.py
+++ alchemy_std/shell.py
@@ -3,13 +3,11 @@ import logging
 
 log = logging.getLogger(__name__)
 
-def runcmd(ctx, args, fail=True, capture=True):
+def _runcmd(cmd, fail=True, capture=False):
     from subprocess import PIPE, Popen
     from alchemy import engine
 
-    args = engine.resolve_list(ctx, args)
-
-    p = Popen(args, stdout = PIPE, stderr=PIPE)
+    p = Popen(cmd, shell=True, stdout = PIPE, stderr=PIPE)
     p.wait()
 
     if not capture:
@@ -22,10 +20,39 @@ def runcmd(ctx, args, fail=True, capture=True):
                 log.info("   2: %s", line.rstrip())
 
         if fail:
-            raise Exception("Command %s failed with rc = %s" % (args, p.returncode))
+            raise Exception("Command %s failed with rc = %s" % (cmd, p.returncode))
             
     return {
         'shell_stdout': p.stdout,
         'shell_stderr': p.stderr,
         'shell_rc': p.returncode
     }
+
+def runcmd(ctx, cmd, fail=True, capture=False):
+    cmd = cmd.format(**ctx.values)
+
+    return _runcmd(cmd, fail=fail, capture=capture)
+
+
+def local_mkdir(dirpath, create_all=True, fail=True):
+    if create_all:
+        cmd = "mkdir -p " + dirpath
+    else:
+        cmd = "mkdir " + dirpath
+
+    return _runcmd(cmd, fail = fail)
+
+def umount(mount_path, options="", fail=True):
+    cmd = "umount {0} {1}".format(mount_path, options)
+    return _runcmd(cmd, fail=fail)
+
+
+def local_nfs_mount(nfs_host, nfs_share, mount_path, options="", create_mount_path=True):
+    if len(options) > 0:
+        options = "-o " + options
+
+    if create_mount_path:
+        local_mkdir(mount_path)
+
+    cmd = "mount -t nfs {0}:{1} {2} {3}".format(nfs_host, nfs_share, mount_path, options)
+    return _runcmd(cmd)
